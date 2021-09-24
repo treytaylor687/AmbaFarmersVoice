297e138f79cefcb3eef590b00acddf173ef2c621
Kunhyuk <klee762@gatech.edu>
2021-09-24 14:13:10 -0400

    Buttons
    buttons

--- Amba/app/src/main/java/com/example/ambafamersvoice/CategoryFragment.java
+++ Amba/app/src/main/java/com/example/ambafamersvoice/CategoryFragment.java
@@ -29,13 +29,108 @@ public class CategoryFragment extends Fragment {
         //ListView listView = (ListView) view.findViewById(R.id.fruitView);
         binding = FragmentCategoryBinding.inflate(inflater, container, false);
         View view = inflater.inflate(R.layout.fragment_category, container, false);
+
+        //all the buttons in categories
         Button btn_aqua = (Button) view.findViewById(R.id.btn_aqua);
+        Button btn_post = (Button) view.findViewById(R.id.btn_post);
+        Button btn_fruit = (Button) view.findViewById(R.id.btn_fruit);
+        Button btn_vege = (Button) view.findViewById(R.id.btn_vege);
+        Button btn_cer = (Button) view.findViewById(R.id.btn_cer);
+        Button btn_fert = (Button) view.findViewById(R.id.btn_fert);
+        Button btn_irri = (Button) view.findViewById(R.id.btn_irri);
+        Button btn_tubers = (Button) view.findViewById(R.id.btn_tubers);
+        Button btn_live = (Button) view.findViewById(R.id.btn_live);
+        Button btn_pest = (Button) view.findViewById(R.id.btn_pest);
+        Button btn_business = (Button) view.findViewById(R.id.btn_business);
+        Button btn_bee = (Button) view.findViewById(R.id.btn_bee);
+
+        btn_post.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent in = new Intent(getActivity(), Video_1.class);
+                startActivity(in);}
+        });
+
+        btn_vege.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent in = new Intent(getActivity(), Video_1.class);
+                startActivity(in);}
+        });
+
+        btn_cer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent in = new Intent(getActivity(), Video_1.class);
+                startActivity(in);}
+        });
+
+        btn_fert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent in = new Intent(getActivity(), Video_1.class);
+                startActivity(in);}
+        });
+
+        btn_irri.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent in = new Intent(getActivity(), Video_1.class);
+                startActivity(in);}
+        });
+
+        btn_tubers.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent in = new Intent(getActivity(), Video_1.class);
+                startActivity(in);}
+        });
+
+        btn_live.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent in = new Intent(getActivity(), Video_1.class);
+                startActivity(in);}
+        });
+
+        btn_pest.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent in = new Intent(getActivity(), Video_1.class);
+                startActivity(in);}
+        });
+
+        btn_business.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent in = new Intent(getActivity(), Video_1.class);
+                startActivity(in);}
+        });
+
+        btn_bee.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent in = new Intent(getActivity(), Video_1.class);
+                startActivity(in);}
+        });
+
+
+
+
+
         btn_aqua.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent in = new Intent(getActivity(), Video_1.class);
                 startActivity(in);}
        });
+
+        btn_fruit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent in = new Intent(getActivity(), Video_1.class);
+                startActivity(in);}
+        });
 
        return view;
    }
