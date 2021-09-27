3f11efe322d0f6a52d4c111d4a596609a790d719
Kunhyuk Lee <klee762@gatech.edu>
2021-09-27 15:25:14 -0400

    fix
    fix

---
+++ Amba/app/src/main/java/com/example/ambafamersvoice/Video_1.java
@@ -0,0 +1,14 @@
+package com.example.ambafamersvoice;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Video_1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_video1);
+    }
+}
