29ddf328dce7ba84540a8a2178d7af7c0b3eb1c0
Pooya Nayebi <pooyanayebi@Pooyas-MacBook-Pro.local>
2021-10-06 13:15:30 -0400

    fertilizer database

--- Amba/app/src/main/java/com/example/ambafamersvoice/FertilizerPage.java
+++ Amba/app/src/main/java/com/example/ambafamersvoice/FertilizerPage.java
@@ -1,14 +1,51 @@
 package com.example.ambafamersvoice;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Picasso;
 
 public class FertilizerPage extends AppCompatActivity {
-
+    ImageView rImage;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fertilizer_page);
+        rImage = findViewById(R.id.rImage);
+        FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
+
+        DatabaseReference databaseReference = firebaseDatabase.getReference();
+
+        DatabaseReference getImage = databaseReference.child("image");
+
+        getImage.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                // getting a DataSnapshot for the location at the specified
+                // relative path and getting in the link variable
+                String link = dataSnapshot.getValue(String.class);
+
+                // loading that data into rImage
+                // variable which is ImageView
+                Picasso.get().load(link).into(rImage);
+            }
+
+            // this will called when any problem
+            // occurs in getting data
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                // we are showing that error message in toast
+                Toast.makeText(FertilizerPage.this, "Error Loading Image", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
